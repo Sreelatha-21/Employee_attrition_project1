e27b8fa3b6becb5216f109274e8ee34148762919
Sreelatha-21 <sreelathareddy807@gmail.com>
2026-04-01 13:46:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 import pandas as pd
 import numpy as np
 #Load the model and label encoder
-model = joblib.load("attrition_model.pkl")
+model = joblib.load(" attrition_model.pkl")
 label_encoder = joblib.load("label_encoder.pkl")
 feature_columns = joblib.load("feature_columns.pkl")
 st.title("Employee Attrition Prediction")
