602f1b553806277f563a3f5731d6d9b67825a768
Sreelatha-21 <sreelathareddy807@gmail.com>
2026-04-01 14:27:41 +0530

    Update app.py
    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 import pandas as pd
 import numpy as np
 #Load the model and label encoder
-model = joblib.load(" attrition_model.pkl")
+model = joblib.load("attrition_model.pkl")
 label_encoder = joblib.load("label_encoder.pkl")
 feature_columns = joblib.load("feature_columns.pkl")
 st.title("Employee Attrition Prediction")
@@ -17,20 +17,25 @@ def get_user_input():
     inputs['Job Satisfaction']=st.sidebar.selectbox("Job Satisfaction",options=[1,2,3,4])
     inputs['OverTime']=st.sidebar.selectbox("Over Time",options=["Yes","No"])
     inputs['DistanceFromHome']=st.sidebar.number_input("Distance From Home",min_value=0,max_value=50,value=10)
-    data ={}
+    
+    data = {}
     for feat in feature_columns:
         if feat in inputs:
             data[feat] = inputs[feat]
         else:
-            data[feat]  = 0
-    return pd .DataFrame(data,index=[0])  
+            data[feat] = 0
+    return pd.DataFrame(data, index=[0])
+
 user_input = get_user_input()
-user_input['OverTime'] = label_encoder.transform(user_input['OverTime'])
+user_input['OverTime'] = label_encoder.transform(
+    user_input['OverTime'])
+
+#Predict the attrition
 if st.button("Predict Attrition"):
     prediction = model.predict(user_input)
+    probability = model.predict_proba(user_input)[0][1]
     if prediction[0] == 1:
         st.error("The employee is likely to leave the company.")
     else:
-        st.succes("The employee is likely to stay with the company.")
+        st.success("The employee is likely to stay with the company.")
     st.info(f"Prediction Probability")
-
